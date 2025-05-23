9ccec568acbd4d3f813f08423949c4541a658544
MINKORO <109242801+MINKORO@users.noreply.github.com>
2025-05-23 20:41:14 +0200

    Mise à jour de main.r

--- main.r
+++ main.r
@@ -1,9 +1,13 @@
-
 add_arr <- function(arr1, arr2)  {
-    return arr1 + arr2
+    return(arr1 + arr2)
 }
 
 arr <- c(8, 8, 8, 4)
 arr2 <- c(8, 8, 8, 4)
+x <- c("A", "B", "C", "D")
+
 print(arr)
 print(add_arr(arr, arr2))
+
+plot(x = 1:4, y = arr, type = "b", xaxt = "n", main = "Test plot", xlab = "X label", ylab = "Y label")
+axis(1, at = 1:4, labels = x)  # Affiche A, B, C, D sur l’axe des x
